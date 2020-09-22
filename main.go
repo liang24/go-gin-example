f5a6dfae27c03c123a5f3dab0780377472a90127
liang24 <380431715@qq.com>
2020-09-22 16:38:14 +0800

    Example07 优雅的重启服务

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/liang24/go-gin-example/pkg/setting"
 	"github.com/liang24/go-gin-example/routers"
 )
 
 func main() {
+	// endless.DefaultReadTimeOut = setting.ReadTimeout
+	// endless.DefaultWriteTimeOut = setting.WriteTimeout
+	// endless.DefaultMaxHeaderBytes = 1 << 20
+	// endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+
+	// server := endless.NewServer(endPoint, routers.InitRouter())
+	// server.BeforeBegin = func(add string) {
+	// 	log.Printf("Actual pid is %d", syscall.Getpid())
+	// }
+
+	// err := server.ListenAndServe()
+	// if err != nil {
+	// 	log.Printf("Server err: %v", err)
+	// }
+
 	router := routers.InitRouter()
 
 	s := &http.Server{
@@ -19,5 +39,23 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
